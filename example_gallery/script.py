a9cdb67cb21901fcd287be73ac807cbb649fc6ed
Justin Parker <justparking@users.noreply.github.com>
2016-02-05 13:30:37 +1100

    Updated to use proper work-flow.

--- example_gallery/script.py
+++ example_gallery/script.py
@@ -1,42 +1,59 @@
 '''Gallery Node'''
 
-### Libraries required by this Node
-from time import sleep
+# This node's events / signal
+
+local_event_AllOn = LocalEvent({"title":"All On"})
+local_event_AllOff = LocalEvent({"title":"All Off", "Caution": "Are you sure you want signal this gallery to turn off?"})
+local_event_MuteOn = LocalEvent({"title":"Mute On"})
+local_event_MuteOff = LocalEvent({"title":"Mute Off"})
 
 
+# This node's actions
 
-### Local events this Node provides
-local_event_AllOn = LocalEvent('{"title":"AllOn","desc":"AllOn","group":"General"}')
-local_event_AllOff = LocalEvent('{"title":"AllOff","desc":"AllOff","group":"General","Caution":"Are you sure?"}')
-local_event_MuteOn = LocalEvent('{"title":"MuteOn","desc":"MuteOn","group":"General"}')
-local_event_MuteOff = LocalEvent('{"title":"MuteOff","desc":"MuteOff","group":"General"}')
+def local_action_AllOn(arg = None):
+  """{"title":"All On", "order": 1}"""
+  print 'Action AllOn requested.'
+  local_event_AllOn.emit()
 
+def local_action_AllOff(arg = None):
+  """{"title": "All Off", "Caution": "Are you sure you want signal this gallery to turn off?", "order": 2}"""
+  print 'Action AllOff requested.'
+  local_event_AllOff.emit()
 
+def local_action_MuteOn(arg = None):
+  """{"title":"Mute On", "order": 3}"""
+  print 'Action MuteOn requested.'
+  local_event_MuteOn.emit()
 
-### Remote events this Node requires
+def local_action_MuteOff(arg = None):
+  """{"title":"Mute Off", "order": 4}"""
+  print 'Action MuteOff requested.'
+  local_event_MuteOff.emit()
+
+  
+# Remote events this node is interested in
+
 def remote_event_AllOn(arg = None):
-  """{"title":"AllOn","desc":"AllOn","group":"General"}"""
+  '{"title":"All On"}'
   print 'Remote event AllOn arrived.'
   local_event_AllOn.emit()
 
 def remote_event_AllOff(arg = None):
-  """{"title":"AllOff","desc":"AllOff","group":"General"}"""
+  '{"title": "AllOff"}'
   print 'Remote event AllOff arrived.'
   local_event_AllOff.emit()
 
 def remote_event_MuteOn(arg = None):
-  """{"title":"MuteOn","desc":"MuteOn","group":"General"}"""
+  '{"title": "MuteOn"}'
   print 'Remote event MuteOn arrived.'
   local_event_MuteOn.emit()
 
 def remote_event_MuteOff(arg = None):
-  """{"title":"MuteOff","desc":"MuteOff","group":"General"}"""
+  '{"title": "MuteOff"}'
   print 'Remote event MuteOff arrived.'
   local_event_MuteOff.emit()
 
 
-
-### Main
+# entry-point
 def main(arg = None):
-  # Start your script here.
-  print 'Nodel script started.'
+  print 'Node started!'
