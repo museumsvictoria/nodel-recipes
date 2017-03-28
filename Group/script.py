3371974ebfa1ba31557eefc779d1c40ecbb15b12
justparking <justparking@gmail.com>
2017-03-28 23:59:31 +1100

    Group: synced with memberStatesAndStatues ingredient.

--- Group/script.py
+++ Group/script.py
@@ -7,87 +7,49 @@ def main(arg=None):
 
 @after_main
 def initMembersSupport():
-  for memberInfo in param_members or []:
+  for memberInfo in lookup_parameter('members') or []:
     initMember(memberInfo)
-  
+
+MODES = ['Action & Signal', 'Signal Only']
+
 param_members = Parameter({'title': 'Members', 'schema': {'type': 'array', 'items': {'type': 'object', 'properties': {
    'name': {'type': 'string', 'order': 1},
-   'slave': {'type': 'boolean', 'title': 'Slave?', 'order': 2},
-   'hasStatus': {'type': 'boolean', 'title': 'Status?', 'order': 3},          
-   'hasPower': {'type': 'boolean', 'title': 'Power?', 'order': 4},
-   'hasMuting': {'type': 'boolean', 'title': 'Muting?', 'order': 5}
- }}}})
-
-membersBySignal = {}  
+   'hasStatus': {'type': 'boolean', 'title': 'Status?', 'order': 3},
+   'power': {'title': 'Power', 'type': 'object', 'order': 4, 'properties': {
+     'mode': {'title': 'Mode', 'type': 'string', 'enum': MODES}}
+   },
+   'muting': {'title': 'Muting', 'type': 'object', 'order': 5, 'properties': {
+     'mode': {'title': 'Mode', 'type': 'string', 'enum': MODES}
+   }
+}}}}})
 
 def initMember(memberInfo):
   name = mustNotBeBlank('name', memberInfo['name'])
-    
-  if memberInfo.get('hasMuting'):
-    initSignalSupport(memberInfo, 'Muting', ['On', 'Off'])
-    
-  if memberInfo.get('hasPower'):
-    initSignalSupport(memberInfo, 'Power', ['On', 'Off'])
-    
+
   if memberInfo.get('hasStatus'):
-    initStatusSupport(memberInfo)
-    
-def initSignal(signalName, states):
-  resultantStates = states + ['Partially %s' % s for s in states]
-  
-  localDesiredSignal = Event('Desired %s' % signalName, {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': states}})
-  
-  localResultantSignal = Event('%s' % signalName, {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': resultantStates}})
-  
-  def adjust(state, propagate):
-    localDesiredSignal.emit(state)
-    
-    # for convenience, just emit the state as the status if no members are configured
-    if isEmpty(param_members):
-      localResultantSignal.emit(state)
-      
-    else:
-      if propagate:
-        for memberName in membersBySignal[signalName]['controlled']:
-          lookup_remote_action('Member %s %s' % (memberName, signalName)).call({'state': state, 'noPropagate': False})
-          
-  localAction = Action('%s' % signalName, lambda arg: adjust(arg, True), {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': states}})
-  
-  localPropagateAction = Action('%s Propagate' % signalName, lambda arg: adjust(arg['state'], not arg.get('noPropagation')), {'group': signalName, 'order': next_seq(), 'schema': {'type': 'object', 'properties': {
-          'state': {'type': 'string', 'enum': states, 'order': 1},
-          'noPropagation': {'type': 'boolean', 'order': 2}}}})
-  
-  return localDesiredSignal, localResultantSignal
+    initStatusSupport(name)
 
-def getMembersInfoOrRegister(signalName, memberName, memberInfo):
-  members = membersBySignal.get(signalName)
-  if members == None:
-    members = { 'slaves': list(),
-                'controlled': list() }
-    membersBySignal[signalName] = members
+  if (memberInfo.get('power') or {}).get('mode') in MODES:
+    initSignalSupport(name, memberInfo['power']['mode'], 'Power', ['On', 'Off'])
     
-  if memberInfo.get('slave'):
-    members['slaves'].append(memberName)
-  else:
-    members['controlled'].append(memberName)    
-    
-  return members
+  if (memberInfo.get('muting') or {}).get('mode') in MODES:
+    initSignalSupport(name, memberInfo['muting']['mode'], 'Muting', ['On', 'Off'])
 
-def initSignalSupport(memberInfo, signalName, states):
-  name = memberInfo['name']
-  
-  members = getMembersInfoOrRegister(signalName, name, memberInfo)
+membersBySignal = {}
+    
+def initSignalSupport(name, mode, signalName, states):
+  members = getMembersInfoOrRegister(signalName, name)
   
   # establish local signals if haven't done so already
   localDesiredSignal = lookup_local_event('Desired %s' % signalName)
   
   if localDesiredSignal == None:
-    localDesiredSignal, localResultantSignal = initSignal(signalName, states)
+    localDesiredSignal, localResultantSignal = initSignal(signalName, mode, states)
   else:
     localResultantSignal = lookup_local_event(signalName)
     
-  # establish a remote action (if not a slave)
-  if not memberInfo.get('slave'):
+  # establish a remote action
+  if mode == 'Action & Signal':
     create_remote_action('Member %s %s' % (name, signalName), {'group': 'Members (%s)' % signalName, 'schema': {'type': 'string', 'enum': states}},
                          suggestedNode=name, suggestedAction=signalName)
   
@@ -95,13 +57,13 @@ def initSignalSupport(memberInfo, signalName, states):
   # signal status states include 'Partially ...' forms
   resultantStates = states + ['Partially %s' % s for s in states]
   
-  localMemberSignal = Event('Member %s %s' % (name, signalName), {'group': 'Members (%s)' % signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': resultantStates}})
+  localMemberSignal = Event('Member %s %s' % (name, signalName), {'title': '"%s" %s' % (name, signalName), 'group': '(advanced)', 'order': 9999+next_seq(), 'schema': {'type': 'string', 'enum': resultantStates}})
   
   def aggregateMemberSignals():
     shouldBeState = localDesiredSignal.getArg()
     partially = False
     
-    for memberName in members['slaves'] + members['controlled']:
+    for memberName in members:
       if lookup_local_event('Member %s %s' % (memberName, signalName)).getArg() != shouldBeState:
         partially = True
         
@@ -111,11 +73,61 @@ def initSignalSupport(memberInfo, signalName, states):
   localDesiredSignal.addEmitHandler(lambda arg: aggregateMemberSignals())
   
   def handleRemoteEvent(arg):
+    if arg == True or arg == 1:
+      arg = 'On'
+    elif arg == False or arg == 0:
+      arg = 'Off'
+    
     localMemberSignal.emit(arg)
   
   create_remote_event('Member %s %s' % (name, signalName), handleRemoteEvent, {'group': 'Members (%s)' % signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': resultantStates}},
-                     suggestedNode=name, suggestedEvent=signalName)
+                      suggestedNode=name, suggestedEvent=signalName)
 
+def initSignal(signalName, mode, states):
+  resultantStates = states + ['Partially %s' % s for s in states]
+  
+  localDesiredSignal = Event('Desired %s' % signalName, {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': states}})
+  
+  localResultantSignal = Event('%s' % signalName, {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': resultantStates}})
+  
+  def handler(complexArg):
+    state = complexArg['state']
+    noPropagate = complexArg.get('noPropagate')
+    
+    localDesiredSignal.emit(state)
+    
+    # for convenience, just emit the state as the status if no members are configured
+    if isEmpty(lookup_parameter('members')):
+      localResultantSignal.emit(state)
+    
+    else:
+      if noPropagate:
+        return
+      
+      for memberName in membersBySignal[signalName]:
+        remoteAction = lookup_remote_action('Member %s %s' % (memberName, signalName))
+        if remoteAction != None:
+          remoteAction.call(state)
+          
+  # create normal action (propagates)
+  Action('%s' % signalName, lambda arg: handler({'state': arg}), {'group': signalName, 'order': next_seq(), 'schema': {'type': 'string', 'enum': states}})
+  
+  # create action with options (e.g. 'noPropagate')
+  Action('%s Extended' % signalName, handler, {'group': '(extended)', 'order': next_seq(), 'schema': {'type': 'object', 'properties': {
+           'state': {'type': 'string', 'enum': states, 'order': 3},
+           'noPropagate': {'type': 'boolean', 'order': 2}}}})
+  
+  return localDesiredSignal, localResultantSignal
+
+def getMembersInfoOrRegister(signalName, memberName):
+  members = membersBySignal.get(signalName)
+  if members == None:
+    members = list()
+    membersBySignal[signalName] = members
+    
+  members.append(memberName)
+
+  return members
 
 STATUS_SCHEMA = { 'type': 'object', 'properties': {
                     'level': { 'type': 'integer', 'order': 1 },
@@ -124,19 +136,22 @@ STATUS_SCHEMA = { 'type': 'object', 'properties': {
 
 EMPTY_SET = {}
   
-def initStatusSupport(memberInfo):
-  name = memberInfo['name']
-  
+def initStatusSupport(name):
   # look up the members structure (assume
-  members = getMembersInfoOrRegister('Status', name, memberInfo)
+  members = getMembersInfoOrRegister('Status', name)
   
   # check if this node has a status yet
   selfStatusSignal = lookup_local_event('Status')
   if selfStatusSignal == None:
     selfStatusSignal = Event('Status', {'group': 'Status', 'order': next_seq(), 'schema': STATUS_SCHEMA})
     
-  # create the status for the member
-  memberStatusSignal = Event('Member %s Status' % name, {'group': 'Members (Status)', 'order': next_seq(), 'schema': STATUS_SCHEMA})
+  # status for the member
+  memberStatusSignal = Event('Member %s Status' % name, {'title': '"%s" Status' % name, 'group': '(advanced)', 'order': 9999+next_seq(), 'schema': STATUS_SCHEMA})
+  
+  # suppression flag?
+  memberStatusSuppressedSignal = Event('Member %s Status Suppressed' % name, {'title': 'Suppress "%s" Status' % name, 'group': '(advanced)', 'order': 9999+next_seq(), 'schema': {'type': 'boolean'}})
+  
+  Action('Member %s Status Suppressed' % name, lambda arg: memberStatusSuppressedSignal.emit(arg), {'title': 'Suppress "%s" Status' % name, 'group': '(advanced)', 'order': 9999+next_seq(), 'schema': {'type': 'boolean'}})
   
   def aggregateMemberStatus():
     aggregateLevel = 0
@@ -145,15 +160,27 @@ def initStatusSupport(memberInfo):
     # for composing the aggegate message at the end
     msgs = []
     
-    for memberName in members['slaves'] + members['controlled']:
+    activeSuppression = False
+    
+    for memberName in members:
+      suppressed = lookup_local_event('Member %s Status Suppressed' % memberName).getArg()
+      
       memberStatus = lookup_local_event('Member %s Status' % memberName).getArg() or EMPTY_SET
       
       memberLevel = memberStatus.get('level')
       if memberLevel == None: # as opposed to the value '0'
+        if suppressed:
+          activeSuppression = True
+          continue
+          
         memberLevel = 99
 
       if memberLevel > aggregateLevel:
-        # raise the level
+        # raise the level (if not suppressed)
+        if suppressed:
+          activeSuppression = True
+          continue
+        
         aggregateLevel = memberLevel
       
       memberMessage = memberStatus.get('message') or 'Has never been seen'
@@ -166,9 +193,13 @@ def initStatusSupport(memberInfo):
     if len(msgs) > 0:
       aggregateMessage = ', '.join(msgs)
       
+    if activeSuppression:
+      aggregateMessage = '%s (*)' % aggregateMessage
+      
     selfStatusSignal.emitIfDifferent({'level': aggregateLevel, 'message': aggregateMessage})
       
   memberStatusSignal.addEmitHandler(lambda arg: aggregateMemberStatus())
+  memberStatusSuppressedSignal.addEmitHandler(lambda arg: aggregateMemberStatus())
   
   def handleRemoteEvent(arg):
     memberStatusSignal.emit(arg)
@@ -194,8 +225,5 @@ def isBlank(s):
 def isEmpty(o):
   if o == None or len(o) == 0:
     return True
-  
-def safely(o):
-  return o if o != None else ''
 
 # convenience functions ---!>
