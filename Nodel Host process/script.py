44f8ea943c2ddbbc739a66745b231cc8a29515e2
justparking <justparking@gmail.com>
2017-04-06 20:05:16 +1000

    Nodel Host process: *update* +'watch' param to watch file and restart on changes (2 min period); 'logToFile' exposed;

--- Nodel Host process/script.py
+++ Nodel Host process/script.py
@@ -1,4 +1,5 @@
 import os
+from java.io import File
 
 DEFAULT_WORKINGDIR = '/opt/bit/site/stablehost'
 DEFAULT_PORT = 0
@@ -10,6 +11,8 @@ param_nodelRelease = Parameter({'title': 'Nodel release', 'desc': 'The full path
                                 'schema': {'type': 'string', 'hint': DEFAULT_NODELRELEASE}})
 param_interface = Parameter({'title': 'Interface', 'desc': 'The interface to bind to.', 'schema': {'type': 'string'}})
 param_nodesRoot = Parameter({'title': 'Nodes root', 'schema': {'type': 'string'}})
+param_watch = Parameter({'title': 'Watch for updates?', 'desc': 'Will watch for updates and recycle the process if detected.', 'schema': {'type': 'boolean'}})
+param_logToFile = Parameter({'title': 'Logging to file? (advanced)', 'schema': {'type': 'boolean'}})
 
 param_nodeFilters = Parameter({'title': 'Node filtering', 'schema': { 'type': 'array', 'items': {
         'type': 'object', 'properties': {
@@ -19,6 +22,9 @@ param_nodeFilters = Parameter({'title': 'Node filtering', 'schema': { 'type': 'a
 local_event_Disabled = LocalEvent({'schema': {'type': 'boolean'}})
 
 process = None # (init. in main)
+
+# holds the resolved Nodel release
+nodelRelease = None
 
 def main():
   console.info('Started!')
@@ -32,6 +38,7 @@ def main():
     console.info('Creating working directory "%s"...' % working)
     os.makedirs(working)
   
+  global nodelRelease
   nodelRelease = param_nodelRelease if param_nodelRelease != None and len(param_nodelRelease)>0 else DEFAULT_NODELRELEASE
   nodelPort = param_port if param_port != None else DEFAULT_PORT
   
@@ -63,6 +70,9 @@ def main():
       
     for excl in exclList:
       params.extend(['-X', excl])
+      
+  if param_logToFile:
+    params.append('-l')
     
   global process
   process = Process(params,
@@ -72,8 +82,34 @@ def main():
   
   console.info('Starting Nodel host... (params are %s)' % params)
   process.start()
+  
+  if param_watch:
+    console.info('Will check for updates every 2 mins.')
+    Timer(lambda: lookup_local_action('CheckForUpdate').call(), 120, 10)
     
 def local_action_Disable(arg=None):
   """{"schema": {"type": "boolean"}}"""
   local_event_Disabled.emit(True)
   process.close()
+  
+local_event_NodelReleaseModified = LocalEvent({'title': 'Nodel release modified', 'schema': {'type': 'string'}})
+  
+def local_action_CheckForUpdate(arg=None):
+  '''{"desc": "Checks for an update", "caution": "If a new update is detected, the process will be recycled."}'''
+  # get the actual modified value
+  actualLastModifiedStr = str(date_instant(File(nodelRelease).lastModified()))
+  
+  # and the previous one
+  lastModifiedStr = local_event_NodelReleaseModified.getArg()
+  
+  if lastModifiedStr == None:
+    local_event_NodelReleaseModified.emit(actualLastModifiedStr)
+    return
+  
+  if actualLastModifiedStr != lastModifiedStr:
+    console.info('File update detected (new modified:%s, old modified:%s); process will be recycled.' % (actualLastModifiedStr, lastModifiedStr))
+    
+    # update before recycling process (drop)
+    local_event_NodelReleaseModified.emit(actualLastModifiedStr)
+    
+    process.drop()
