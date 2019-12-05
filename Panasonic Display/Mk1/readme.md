a6d7cc5d3739df975ccedf65d2c41d32507b03dc
justparking <justparking@gmail.com>
2019-12-05 23:56:42 +1100

    panasonic *update*: reworked to support both serial and network protocol (protected and unprotected); power-on-no-signal check; tidyup

--- Panasonic Display/Mk1/readme.md
+++ Panasonic Display/Mk1/readme.md
@@ -1,3 +1,3 @@
 Tested with at least the following models:
  * TH-55LF6
- 
+ * 32EF1W
--- Panasonic Display/Mk1/script.py
+++ Panasonic Display/Mk1/script.py
@@ -21,14 +21,23 @@
 
 param_disabled = Parameter({'title':'Disabled?', 'order': next_seq(), 'schema': { 'type': 'boolean' }})
 param_ipAddress = Parameter({'title':'IP address', 'order': next_seq(), 'schema': { 'type': 'string' }})
+
 DEFAULT_PORT = 1024
-param_port = Parameter({'title': 'Port', 'order': next_seq(), 'schema': { 'type':'integer', 'hint': '(default is %s' % DEFAULT_PORT }})
+param_port = Parameter({'title': 'Port', 'order': next_seq(), 'schema': { 'type':'integer', 'hint': '(default %s)' % DEFAULT_PORT }})
+
+DEFAULT_USERNAME = 'admin1'
+param_username = Parameter({'order': next_seq(), 'schema': {'type': 'string', 'hint': '(default %s)' % DEFAULT_USERNAME}})
+
+DEFAULT_PASSWORD = 'panasonic'
+param_password = Parameter({'order': next_seq(), 'schema': {'type': 'string', 'hint': '(default %s)' % DEFAULT_PASSWORD}})
 
 local_event_RawPower = LocalEvent({'group': 'Power', 'schema': {'type': 'string', 'enum': ['On', 'Off']}})
 local_event_DesiredPower = LocalEvent({'group': 'Power', 'schema': {'type': 'string', 'enum': ['On', 'Off']}})
 local_event_Power = LocalEvent({'group': 'Power', 'schema': {'type': 'string', 'enum': ['On', 'Partially On', 'Partially Off', 'Off']}})
 
 local_event_TCPState = LocalEvent({'title': 'TCP state', 'order': 1, 'group': 'TCP', 'schema': {'type': 'string', 'order': 1, 'enum': ['Connected', 'Disconnected']}})
+
+import hashlib # for authentication
 
 def main():
   if param_disabled or len((param_ipAddress or '').strip()) == 0:
@@ -59,22 +68,27 @@ def syncPower():
     return
   
   arg = local_event_DesiredPower.getArg()
+  raw = local_event_RawPower.getArg()
+  
+  if arg == raw:
+    log(2, '(POWER: desired and raw are both "%s"; nothing to do)' % arg)
+    return
   
   log(2, 'POWER: forcing power to %s' % arg)
   
   if arg == 'On':
-    tcp.request('\x02PON\x03', handle_powerResp)
+    makeRequest('PON', handle_powerResp)
     
   elif arg == 'Off':
-    tcp.request('\x02POF\x03', handle_powerResp)
+    makeRequest('POF', handle_powerResp)
   
 timer_powerSyncer = Timer(lambda: syncPower.call(), 10, 0)  
   
 def handle_powerResp(resp):
-  if resp in ['QPW:1', 'PON']:
+  if resp in ['QPW:1', 'PON', '1']:
     local_event_RawPower.emit('On')
       
-  elif resp in ['QPW:0', 'POF']:
+  elif resp in ['QPW:0', 'POF', '0']:
     local_event_RawPower.emit('Off')
       
   else:
@@ -82,7 +96,7 @@ def handle_powerResp(resp):
 
 @local_action({'group': 'Power', 'order': next_seq()})
 def PollPower(arg=None):
-  tcp.request('\x02QPW\x03', handle_powerResp)
+  makeRequest('QPW', handle_powerResp)
   
 poller_timer = Timer(lambda: lookup_local_action('PollPower').call(), 10, 5)  
   
@@ -162,19 +176,20 @@ def PollInput(arg=None):
       local_event_RawInput.emit(argPart)
 
     else:
-      console.warn('Unexpected feedback from input request. resp:[%s]' % resp)
+      # presuming the resp is the input
+      local_event_RawInput.emit(resp)
       
-  rawInputArg = local_event_RawInput.getArg()
+  rawPowerArg = local_event_RawPower.getArg()
   
-  if rawInputArg in ['Off', 'Partially Off']:
+  if rawPowerArg in ['Off', 'Partially Off']:
     local_event_RawInput.emit('Off')
       
-  if rawInputArg != 'On':
+  if rawPowerArg != 'On':
     log(2, 'INPUT: Power is not On, so not querying input')
     return
 
   log(2, 'INPUT: Querying input (power is On so can)')
-  tcp.request('\x02QMI\x03', handleResp)
+  makeRequest('QMI', handleResp)
   
 poller_timer = Timer(lambda: lookup_local_action('PollInput').call(), 10, 5)  
   
@@ -192,19 +207,107 @@ def SyncInput():
     log(2, '(INPUT: power is off; nothing to do)')
     return
   
-  # otherwise attempt to set within the minute
+  # otherwise attempt to set within the minute if required
   desiredArg = local_event_DesiredInput.getArg()
+  raw = local_event_RawInput.getArg()  
+  
+  if desiredArg == raw:
+    log(2, 'INPUT: (desired and raw are both "%s"; nothing to do)' % desiredArg)
+    return
+  
   log(2, 'INPUT: forcing INPUT to %s' % desiredArg)
   
   def handleResp(resp):
     if resp == 'IMS':
       local_event_RawInput.emit(desiredArg)
   
-  tcp.request('\x02IMS:%s\x03' % desiredArg, handleResp)
+  makeRequest('IMS:%s' % desiredArg, handleResp)
   
 timer_inputSyncer = Timer(lambda: SyncInput.call(), 10, 0)
 
 # ---]
+
+# <!-- signal check
+
+#          QSF         # signal check
+#          QSF:M--------------------    # no signal
+#          QSF:M-1125(1080)/50p         # signal
+
+local_event_SignalStatus = LocalEvent({'group': 'Signal', 'order': next_seq(), 'schema': {'type': 'string'}})
+
+local_event_FirstNoSignal = LocalEvent({'group': 'Signal', 'order': next_seq(), 'schema': {'type': 'string'}})
+
+@local_action({'group': 'Signal', 'order': next_seq()})
+def PollSignalStatus(arg=None):
+  def handleResp(resp):
+    if not resp.startswith('M'):
+      warn(1, 'unexp resp polling signal status')
+      return
+    
+    elif '----' in resp:
+      if local_event_SignalStatus.getArg() != 'NO SIGNAL':
+        local_event_FirstNoSignal.emit(str(date_now()))
+        local_event_SignalStatus.emit('NO SIGNAL')
+    
+    else:
+      local_event_SignalStatus.emit(resp)
+      
+  log(2, 'SIGNALSTATUS: Querying')
+  makeRequest('QSF', handleResp)
+  
+# seems to be safe to poll even while power is Off
+poller_SignalStatus = Timer(lambda: PollSignalStatus.call(), 10, 5)
+
+# -->
+
+# [ Protocol ---
+
+# e.g. on_connect
+#      >> NTCONTROL 1 3f020e71
+# 
+# hash(__admin_user_name__:__password__:__randnumber__)
+# commands:
+# HASH + '00' + command + CR
+
+_authPrefix = None # e.g. None                   -- Pure serial
+                   #      'caabbccddeeaa00'      -- network protected
+                   #      '00'                   -- network unprotected
+
+# assumes network 
+def prepareAuth(line): 
+  # line e.g. "NTCONTROL 1 3f020e71"
+  parts = line.split(' ')
+  
+  global _authPrefix
+  
+  if parts[1] == '0':
+    console.info('Using Panasonic network protocol unprotected')
+    _authPrefix = '00'
+    
+  elif parts[1] == '1':
+    console.info('Using Panasonic network protocol protected')  
+    
+    randomPart = parts[2]
+    toBeHashed = '%s:%s:%s' % (param_username or DEFAULT_USERNAME, param_password or DEFAULT_PASSWORD, randomPart)
+    hsh = hashlib.md5(toBeHashed).hexdigest()
+    _authPrefix = '%s00' % hsh
+    
+def makeRequest(cmd, onResp):
+  def trapResp(data):
+    lastReceive[0] = system_clock()
+    log(2, 'panasonic_recv: [%s]' % data)
+    onResp(data)
+      
+  log(2, 'panasonic_send: [%s]' % cmd)
+  if _authPrefix == None:
+    tcp.request('\x02%s\x03' % cmd, trapResp)
+    
+  else:
+    # add '00 suffix on send and ...
+    # drop the '00' prefix from response for network protocol (protected and unprotected)
+    tcp.request('%s%s\r' % (_authPrefix, cmd), lambda resp: trapResp(resp[2:])) 
+
+# ]
 
 # [ TCP ---
 
@@ -212,17 +315,21 @@ def connected():
   console.info('Connected!')
   local_event_TCPState.emit('Connected')
   
-def received(data):
-  lastReceive[0] = system_clock()
+  tcp.clearQueue()
   
-  log(2, 'RECV: [%s]' % data)
+def received(data):
+  log(3, 'tcp_recv: [%s]' % data)
+  
+  if 'NTCONTROL ' in data:
+    prepareAuth(data)
   
 def sent(data):
-  log(2, 'SENT: [%s]' % data)
+  log(3, 'tcp_sent: [%s]' % data)
   
 def disconnected():
   console.warn('TCP disconnected')
   local_event_TCPState.emit('Disconnected')
+  
   
 def timeout():
   console.warn('TCP timeout!')
@@ -236,8 +343,8 @@ tcp = TCP(connected=connected,
           disconnected=disconnected, 
           timeout=timeout,
           sendDelimiters='', 
-          receiveDelimiters='\x03')
-
+          receiveDelimiters='\x03\r\n') # \x03 for pure serial
+                                        # \r\n for Panasonic network protocol
 
 # status ---
 
@@ -267,24 +374,32 @@ def statusCheck():
       
     else:
       previousContact = date_parse(previousContactValue)
-      roughDiff = (now.getMillis() - previousContact.getMillis())/1000/60
-      if roughDiff < 60:
-        message = 'Off the network for approx. %s mins' % roughDiff
-      elif roughDiff < (60*24):
-        message = 'Off the network since %s' % previousContact.toString('h:mm:ss a')
-      else:
-        message = 'Off the network since %s' % previousContact.toString('h:mm:ss a, E d-MMM')
+      message = 'Off the network %s' % formatPeriod(previousContact)
       
     local_event_Status.emit({'level': 2, 'message': message})
     return
     
-  else:
-    local_event_Status.emit({'level': 0, 'message': 'OK'})
+  if local_event_Power.getArg() == 'On' and local_event_SignalStatus.getArg() == 'NO SIGNAL':
+    local_event_Status.emit({'level': 1, 'message': 'Power On but no signal %s' % formatPeriod(local_event_FirstNoSignal.getArg())})
+    return
   
-  local_event_LastContactDetect.emit(str(now))  
+  local_event_Status.emit({'level': 0, 'message': 'OK'})
+  
+  local_event_LastContactDetect.emit(str(now))
   
 status_check_interval = 75
 status_timer = Timer(statusCheck, status_check_interval)
+
+def formatPeriod(dateObj):
+  if dateObj == None:      return 'for unknown period'
+  
+  now = date_now()
+  diff = (now.getMillis() - dateObj.getMillis()) / 1000 / 60 # in mins
+  
+  if diff == 0:             return 'for <1 min'
+  elif diff < 60:           return 'for <%s mins' % diff
+  elif diff < 60*24:        return 'since %s' % dateObj.toString('h:mm:ss a')
+  else:                     return 'since %s' % dateObj.toString('E d-MMM h:mm:ss a')
 
 # <!-- logging
 
