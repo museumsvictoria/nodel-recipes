aec48be891271c6dfed49fe0096cf0d8525674d3
Justin Parker <justparking@users.noreply.github.com>
2021-11-24 12:25:37 +1100

    Serveredge Switched PDU: *update* "Power Off" guarding, IP addressing through bindings and more (see recipe header). (#62)
    Implemented by: automatic.com.au thanks to ACMI

--- Serveredge Switched PDU/Mk2/script.py
+++ Serveredge Switched PDU/Mk2/script.py
@@ -1,13 +1,23 @@
-'''Pure SNMP based control of the Servedge PDUs - see script for additional resources'''
+'''
+Pure SNMP-based control of the **Servedge** PDUs.
 
-# Useful resources:
-# - PDUMIB-201010105.mib (part of recipe) file contains the MIB file for the Serveredge PDUs
-# - sourceforge.net/projects/jmibbrowser project is a decent little MIB browser
+Useful resources:
 
-# TODO:
-# - use SNMP to do the port labelling
-# - current thresholds into status monitoring
+* **PDUMIB-201010105.mib** file contains the MIB file for the Serveredge PDUs (not needed for operation, for documentation only)
+* sourceforge.net/projects/jmibbrowser project is a decent little MIB browser
 
+_rev 2b_
+
+* port labelling exposed
+* less console noise
+* optional IP addressing using remote binding
+* enforcement period after action use
+* "Power Off" Guarding to accommodate graceful / sequenced shutdown
+
+NOTE / TODO:
+
+  * "Power Off" Guarding requires at least one successful connection before Bindings section can be filled in.
+'''
 
 # <! -- imports
 
@@ -29,14 +39,27 @@ from org.snmp4j import Snmp, PDU
 
 param_disabled = Parameter({'desc': 'Disables this node', 'schema': {'type': 'boolean'}})
 
-param_ipAddress = Parameter({'schema': {'type': 'string' }})
-                              
+param_ipAddress = Parameter({'schema': {'type': 'string', 'hint': '(overrides bindings use)' }})
+
+local_event_IPAddress = LocalEvent({ 'group': 'Addressing', 'order': next_seq(), 'schema': { 'type': 'string' }})
+
+def remote_event_IPAddress(arg):
+  if is_blank(param_ipAddress) and arg != local_event_IPAddress.getArg():
+    # update IP address only if parameter is not set
+    console.warn('IP address updated to "%s"; will restart node' % arg)
+    local_event_IPAddress.emit(arg)
+    _node.restart()
 
 DEFAULT_PORT = SnmpConstants.DEFAULT_COMMAND_RESPONDER_PORT
 param_port = Parameter({'schema': {'type': 'integer', 'hint': '%s (default)' % DEFAULT_PORT}})
 
 DEFAULT_COMMUNITY = 'public'
 param_community = Parameter({'schema': {'type': 'string', 'hint': '%s (default)' % DEFAULT_COMMUNITY}})
+
+param_powerOffGuarding = Parameter({ 'title': '"Power Off" Guarding', 'order': next_seq(),
+                                     'schema': { 'type': 'array', 'items': { 'type': 'object', 'properties': {
+                                       'port': { 'tite': 'PDU port', 'type': 'integer', 'order': 1, 'desc': 'Prevents Power Off until nodes/devices are gracefully shutdown. Bindings need to be filled in after connection to device.' },
+                                       'count': { 'type': 'integer', 'order': 2, 'desc': 'How many nodes to aggregate Power signals of.' }}}}})
 
 # -->
 
@@ -47,21 +70,31 @@ _target.setVersion(SnmpConstants.version1)
 _target.setRetries(2)
 _target.setTimeout(1000)
 
+_timers = list()
+
 def main():
-  if param_disabled or not param_ipAddress:
+  ipAddress = param_ipAddress if not is_blank(param_ipAddress) else local_event_IPAddress.getArg()
+  local_event_IPAddress.emit(ipAddress)
+
+  if param_disabled or is_blank(ipAddress):
     console.warn('Disabled or IP address not specified; nothing to do')
     return
   
   # complete the init of the target
-  _target.setAddress(UdpAddress('%s/%s' % (param_ipAddress, param_port or DEFAULT_PORT)))
+  console.info('Using IP address "%s"' % ipAddress)
+  _target.setAddress(UdpAddress('%s/%s' % (ipAddress, param_port or DEFAULT_PORT)))
   _target.setCommunity(OctetString(param_community or DEFAULT_COMMUNITY))
   
   # kick-off timer to poll the outlet status
-  Timer(lambda: lookup_local_action('snmpLookupOID').call({'oid': OUTLETSTATUS_OID,
-                                                           'signal': 'rawOutletStatus'}), 30, 2)
+  Timer(lambda: snmpLookupOID.call({ 'oid': OUTLETSTATUS_OID, 'signal': 'rawOutletStatus' }), 30, 2)
+
+  # have IP address so can start timers
+  for t in _timers:
+    t.start()
+  timer_syncer.start()
   
   # and trap the feedback
-  local_event_rawOutletStatus.addEmitHandler(handleOutletStatusResult)
+  local_event_RawOutletStatus.addEmitHandler(handleOutletStatusResult)
   
   
 
@@ -83,13 +116,15 @@ def main():
 OUTLETSTATUS_OID = '1.3.6.1.4.1.17420.1.2.9.1.13.0'
 
 # e.g. "1,1,-1,-1,-1,-1,-1,-1"
-local_event_rawOutletStatus = LocalEvent({'title': 'Outlet Status', 'group': 'Raw Feedback', 'order': next_seq(), 'schema': {'type': 'string'}})
+local_event_RawOutletStatus = LocalEvent({'title': 'Outlet Status', 'group': 'Raw Feedback', 'order': next_seq(), 'schema': {'type': 'string'}})
 
 def handleOutletStatusResult(result):
   # split the result
   for index, portStatus in enumerate(result.split(',')):
     if portStatus in ['-2', '0', '1']:
       tryInitPort(index+1, portStatus)
+      
+  prepareForGuarding()
 
 def portStatusToName(portStatus):
   if portStatus == '0':
@@ -112,53 +147,110 @@ def tryInitPort(portNum, portStatus):
     # already initialised, just emit signal
     rawPortSignal.emit(portStatusToName(portStatus))
     return
+
+  # init labelling (uses "pdu01Outlet2Config" e.g. "COMPUTER,0,0,0,0")
+  rawConfigSignalName = 'Port %s Raw Config' % portNum
+  rawConfigSignal = create_local_event(rawConfigSignalName, {'group': 'Raw Feedback', 'order': next_seq(), 'schema': {'type': 'string'}})
+  labelSignal = create_local_event('Port %s Label' % portNum, {'group': 'Labels', 'order': next_seq(), 'schema': {'type': 'string'}})
+  
+  def getPortConfig():
+    return doSnmpLookupOID('1.3.6.1.4.1.17420.1.2.9.1.14.%s.0' % portNum, rawConfigSignalName)
+    
+  Timer(getPortConfig, 5*60, 2)
+  
+  rawConfigSignal.addEmitHandler(lambda arg: labelSignal.emit(arg.split(',')[0]))
+  
+  # retrieve labelling now
+  configStr = getPortConfig()    
   
   # initialising signals...
+  label = configStr.split(',')[0]
   rawSignal     = create_local_event('Port %s Raw Power' % portNum,     {'group': 'Raw Feedback', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off', 'Lost']}})
-  powerSignal   = create_local_event('Port %s Power' % portNum,         {'group': 'Power',        'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off', 'Partially On', 'Partially Off']}})
-  desiredSignal = create_local_event('Port %s Desired Power' % portNum, {'group': 'Power',        'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}})
+  powerSignal   = create_local_event('Port %s Power' % portNum,         {'group': 'Power%s' % (' ("%s")' % label if label else ''),        'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off', 'Partially On', 'Partially Off']}})
+  desiredSignal = create_local_event('Port %s Desired Power' % portNum, {'group': 'Power%s' % (' ("%s")' % label if label else ''),        'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off', 'Force Off']}})
   
-  def computerPower(ignore):
+  def computePower(ignore):
     desiredArg = desiredSignal.getArg()
     rawArg = rawSignal.getArg()
     
     if desiredArg == None:
       powerSignal.emit(rawArg)
+      return
+
+    # strip 'Forced' if 'Forced Off', that'll be used later
+    desiredArg = 'Off' if 'Off' in desiredArg else 'On'
     
-    elif desiredArg == rawArg:
+    if desiredArg == rawArg:
       powerSignal.emit(desiredArg)
       
     else:
       powerSignal.emit('Partially %s' % desiredArg)
       
-  desiredSignal.addEmitHandler(computerPower)
-  rawSignal.addEmitHandler(computerPower)
+  desiredSignal.addEmitHandler(computePower)
+  rawSignal.addEmitHandler(computePower)
   
   # and action...
   def handlePower(arg):
+    console.info('Port %s Power(%s)' % (portNum, arg))
     desiredSignal.emit(arg)
-    
+    log(1, 'handlePower_port%s: setting delay to 0.1s and interval 5s' % portNum)
+    timer_syncer.setDelayAndInterval(0.1, 5) # kick off immediately (100ms if batching is required), use 5s interval during enforcement period
+  
+  powerAction = create_local_action('Port %s Power' % portNum, handlePower, {'group': 'Power%s' % (' ("%s")' % label if label else ''), 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off', 'Force Off']}})
+  create_local_action('Port %s Power On' % portNum, lambda ignore: powerAction.call('On'), { 'group': 'Power%s' % (' ("%s")' % label if label else ''), 'order': next_seq() })
+  create_local_action('Port %s Power Off' % portNum, lambda ignore: powerAction.call('Off'), { 'group': 'Power%s' % (' ("%s")' % label if label else ''), 'order': next_seq() })
+
+def sync_outlet_states():
     # get the existing status of all the ports
-    rawOutletStatus = lookup_local_event('rawOutletStatus').getArg()
-    
+    rawOutletStatus = local_event_RawOutletStatus.getArg()
     if is_blank(rawOutletStatus):
       return
     
-    # recompose with the new state
-    
     parts = rawOutletStatus.split(',')
+
+    # recompose with the new states, only if they've been recently controlled using an action
+    nowMillis = date_now().getMillis()
+
+    recentAction = False
+
+    for portNum in range(1, len(parts) + 1): # go through all known ports
+      action = lookup_local_action('Port %s Power' % portNum)
+      if action == None:
+        break # have come to the end of ports
+
+      timestamp = action.getTimestamp()
+      if not timestamp or (nowMillis - timestamp.getMillis()) > 5*60000:
+        continue # last action was more than 5 mins ago so give up on this port
+
+      recentAction = True
+      desired = lookup_local_event('Port %s Desired Power' % portNum).getArg()
+
+      if desired == 'On':
+        parts[portNum-1] = '1'
+
+      elif desired == 'Force Off':
+        parts[portNum-1] = '0'
+      
+      elif desired == 'Off':
+        powerOffGuard = lookup_local_event('Port %s Power Off Guarded' % portNum)
+        if powerOffGuard and powerOffGuard.getArg():
+          console.warn('POWER: want to turn Port %s Power Off but Power Locked Out for now' % portNum)
+        else:
+          # is not guarded so safe to turn off
+          parts[portNum-1] = '0'
+      
+    # set SNMP if changed from original
+    newStatus = ','.join(parts)
+    if newStatus != rawOutletStatus:
+      log(1, 'timer_syncer: status strings changed; forcing SNMP; "%s" (orig was %s)' % (newStatus, rawOutletStatus))
+      snmpSetOID.call({'oid': OUTLETSTATUS_OID, 'value': newStatus, 'signal': 'rawOutletStatus'})
     
-    if arg == 'On':
-      parts[portNum-1] = '1'
-      
-    elif arg == 'Off':
-      parts[portNum-1] = '0'
-      
-    lookup_local_action('snmpSetOID').call({'oid': OUTLETSTATUS_OID, 'value': ','.join(parts), 'signal': 'rawOutletStatus'})
-  
-  powerAction = create_local_action('Port %s Power' % portNum, handlePower, {'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}})
-  
-  
+    if not recentAction:
+      log(1, 'timer_syncer: no actions within last 5 mins, setting interval to 60s')
+      timer_syncer.setInterval(60) # is safe to back-off to 1 min intervals
+
+timer_syncer = Timer(sync_outlet_states, 20, stopped=True) # every 20 seconds but mainly on action
+
   
 # power --!>
 
@@ -168,25 +260,25 @@ def tryInitPort(portNum, portStatus):
 #
 # Name   : pdu01Value    Parent : pdu01Entry
 # Number : 11            Access :  read-only  
-# Syntax :  INTEGER      # Status :  mandatory
+# Syntax :  INTEGER      Status :  mandatory
 # Description : 
 #  "Indicate the current of PDU-01detect."
 # Type :0
 
 local_event_Current = LocalEvent({'title': 'Current', 'group': 'Monitoring', 'order': next_seq(), 'schema': {'type': 'string'}})
-Timer(lambda: lookup_local_action('snmpLookupOID').call({'oid': '.1.3.6.1.4.1.17420.1.2.9.1.11.0', 'signal': 'Current'}), 10, 2)
+_timers.append(Timer(lambda: snmpLookupOID.call({'oid': '.1.3.6.1.4.1.17420.1.2.9.1.11.0', 'signal': 'Current'}), 10, 2, stopped=True))
 
 
 # Firmware
 
 local_event_Firmware = LocalEvent({'title': 'Firmware', 'group': 'Device Info', 'order': next_seq(), 'schema': {'type': 'string'}})
-Timer(lambda: lookup_local_action('snmpLookupOID').call({'oid': '.1.3.6.1.4.1.17420.1.2.4.0', 'signal': 'Firmware'}), 5*60, 2)
+_timers.append(Timer(lambda: snmpLookupOID.call({'oid': '.1.3.6.1.4.1.17420.1.2.4.0', 'signal': 'Firmware'}), 5*60, 2, stopped=True))
 
 
 # MAC Address
 
 local_event_MACAddress = LocalEvent({'title': 'MAC address', 'group': 'Device Info', 'order': next_seq(), 'schema': {'type': 'string'}})
-Timer(lambda: lookup_local_action('snmpLookupOID').call({'oid': '.1.3.6.1.4.1.17420.1.2.3.0', 'signal': 'MACAddress'}), 5*60, 2)
+_timers.append(Timer(lambda: snmpLookupOID.call({'oid': '.1.3.6.1.4.1.17420.1.2.3.0', 'signal': 'MACAddress'}), 5*60, 2, stopped=True))
 
 
 # etc.
@@ -194,14 +286,15 @@ Timer(lambda: lookup_local_action('snmpLookupOID').call({'oid': '.1.3.6.1.4.1.17
 # other values -->
 
 # <!-- raw SNMP
+@local_action({ 'group': 'SNMP', 'order': 100, 'schema': { 'type': 'object', 'properties': { 
+                  'oid': { 'type': 'string', 'order': 1 },
+                  'signal': { 'type': 'string', 'order': 2 }}}})
+def snmpLookupOID(arg):
+  doSnmpLookupOID(arg['oid'], arg['signal'])
 
-def local_action_snmpLookupOID(arg):
-  '''{'group': 'SNMP', 'order': 100, 'schema': {'type': 'object', 'properties': { 
-        'oid':    {'type': 'string', 'order': 1},
-        'signal': {'type': 'string', 'order': 2}
-     }}}'''
+def doSnmpLookupOID(oid, signalName):  
   pdu = PDU()
-  pdu.add(VariableBinding(OID(arg['oid'])))
+  pdu.add(VariableBinding(OID(oid)))
   pdu.setType(PDU.GET)
   
   respEvent = NodelSnmp.shared().get(pdu, _target)
@@ -213,7 +306,7 @@ def local_action_snmpLookupOID(arg):
   respPDU = respEvent.getResponse()
   
   if respPDU == None:
-    console.warn('response PDU was empty')
+    warn(1, 'response was empty')
     return
   
   lastReceive[0] = system_clock()
@@ -230,16 +323,17 @@ def local_action_snmpLookupOID(arg):
   
   result = str(respPDU.getVariableBindings()[0].getVariable())
   
-  signal = lookup_local_event(arg['signal'])
+  signal = lookup_local_event(signalName)
   if signal != None:
     signal.emit(result)
-  
-def local_action_snmpSetOID(arg):
-  '''{'group': 'SNMP', 'order': 100, 'schema': {'type': 'object', 'properties': { 
-        'oid':    {'type': 'string', 'order': 1},
-        'value':  {'type': 'string', 'order': 2},
-        'signal': {'type': 'string', 'order': 3}
-     }}}'''
+
+  return result
+
+@local_action({ 'group': 'SNMP', 'order': 100, 'schema': { 'type': 'object', 'properties': { 
+                  'oid':    {'type': 'string', 'order': 1},
+                  'value':  {'type': 'string', 'order': 2},
+                  'signal': {'type': 'string', 'order': 3}}}})
+def snmpSetOID(arg):
   pdu = PDU()
   pdu.add(VariableBinding(OID(arg['oid']), OctetString(arg['value'])))
   pdu.setType(PDU.SET)
@@ -253,7 +347,7 @@ def local_action_snmpSetOID(arg):
   respPDU = respEvent.getResponse()
   
   if respPDU == None:
-    console.warn('response PDU was empty')
+    warn(1, 'resp was empty')
     return
   
   errStatus = respPDU.getErrorStatus()
@@ -272,9 +366,49 @@ def local_action_snmpSetOID(arg):
 
 # ---!>
 
-# <!-- status
+# <!-- Power Off guarding
 
-# status  ----
+# (called once PDU polled at least once)
+def prepareForGuarding():
+  for i in param_powerOffGuarding or EMPTY:
+    initPowerOffGuarding(i['port'], i['count'])
+
+def initPowerOffGuarding(portNum, count):
+  if not count:
+    return
+  
+  label = lookup_local_event('Port %s Label' % portNum).getArg()
+  
+  poweroffGuardedName = 'Port %s Power Off Guarded' % portNum
+  if lookup_local_event(poweroffGuardedName):
+    return
+  
+  powerOffGuardedEvent = create_local_event('Port %s Power Off Guarded' % portNum, { 'group': 'Power%s' % (' ("%s")' % label if label else ''), 'order': next_seq(), 'schema': { 'type': 'boolean' }})
+
+  events = list()
+
+  def handler(arg):
+    prev = powerOffGuardedEvent.getArg()
+    
+    # aggregate all remote args, True if *any* is active
+    value = any([ not is_fully_off(e.getArg()) for e in events ]) 
+
+    powerOffGuardedEvent.emit(value)
+
+    if prev != value:
+      _lastGuardOffChange = system_clock()
+
+  console.info('%s set up for Power Off guard; ensure bindings have been set' % count)
+  
+  for i in range(1, (count or 0) + 1):
+    events.append(create_remote_event('Port %s Guarding Power Off %s' % (portNum, i), handler))
+    
+def is_fully_off(value):
+  return value == False or value in ['Off', 'off']    
+
+# Power Off guarding -->
+
+# <!-- status
 
 local_event_Status = LocalEvent({'group': 'Status', 'order': 9990, "schema": { 'title': 'Status', 'type': 'object', 'properties': {
         'level': {'title': 'Level', 'order': next_seq(), 'type': 'integer'},
