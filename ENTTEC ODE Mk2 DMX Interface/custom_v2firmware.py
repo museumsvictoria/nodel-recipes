f0f06e4efc02ccdce48a559ea9048583313487f7
scroix <julien.dsc@artprocessors.net>
2023-02-06 11:02:36 +1100

    Add custom script to support V2 firmware

---
+++ ENTTEC ODE Mk2 DMX Interface/custom_v2firmware.py
@@ -0,0 +1,45 @@
+import urllib2
+
+FIRMWARE_VERSIONS = ['V1.x', 'V2.x']
+DEFAULT_FIRMWARE_VERSION = FIRMWARE_VERSIONS[0]
+
+param_version = Parameter({'title': 'Firmware version', 'schema': {'type': 'string', 'hint': '%s' % DEFAULT_FIRMWARE_VERSION, 'enum': FIRMWARE_VERSIONS}, 'order': next_seq()})
+
+# An alternative to the default get_url function for V2.x firmware.
+# It (strangely) uses HTTP/0.9 to communicate with the device.
+def v2firmware_get_url(address):
+
+    if param_version == 'V2.x':
+
+      # Ignore default recipe address
+      address = 'http://%s:80/index.html?buffer1.cgi' % _ipAddress
+
+      # Set the HTTP version to HTTP/0.9
+      req = urllib2.Request("%s" % address)
+      req.add_header("Accept", "HTTP/0.9")
+
+      # Send the request
+      response = urllib2.urlopen(req)
+
+      # Get the response status code
+      status_code = response.code
+      log(2, ("Status code: %s" % status_code))
+
+      # Get the response body
+      response_body = response.read()
+
+      # Close the connection
+      response.close()
+
+      # Return the body
+      return response_body
+
+    else:
+
+      # Use the default recipe fucntion
+      return original_get_url(address)
+
+# Replace the default get_url function with the custom one
+original_get_url = globals().get("get_url", None)
+def get_url(address):
+    return v2firmware_get_url(address) if param_version == 'V2.x' else original_get_url(address)
